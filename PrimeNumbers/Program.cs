709c42147fea0f8168f8c64be765109e90992f4d
Ronald Rowe <ronaldgilesrowe@gmail.com>
2022-06-03 10:09:27 -0500

    use factorlist.count instead  index of last element in flist to reduce steps, increase speed, helped but too many steps still while loop with incrementer outdid it this time.

--- PrimeNumbers/Program.cs
+++ PrimeNumbers/Program.cs
@@ -1,29 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Diagnostics;
-List<int> factorList = new() { 2};
-List<int> primeList = new() { 2};
+List<int> factorList = new() { };
+List<int> primeList = new() { 2 };
 int maxNum;
+int n = 0;
 
-Stopwatch stopWatch = new Stopwatch();
 
+Stopwatch stopWatch = new ();
+Stopwatch stopWatch2 = new();
 
 Console.Write("Enter Whole Number Greater than 1: ");
 
 maxNum = Convert.ToInt32(Console.ReadLine());
 stopWatch.Start();
+stopWatch2.Start();
 
 foreach (int startNum in Enumerable.Range(1,maxNum-2).Select(x => x+2))
 {
-    foreach (int p in primeList.Skip(1+primeList.IndexOf(factorList[^1])))
+    while (n <  primeList.Count)
     {
-        if (p >= Math.Sqrt(startNum) + 1)
+        if (primeList[n] >= Math.Sqrt(startNum) + 1)
         {
             break;
         }
         else
         {
-            factorList.Add(p);
+            factorList.Add(primeList[n]);
+            n ++;
         }
     }
     foreach (int f in factorList)
@@ -48,16 +52,25 @@ foreach (int startNum in Enumerable.Range(1,maxNum-2).Select(x => x+2))
     
 }
 
-foreach (int p in primeList)
-{
-    Console.Write(p+", ");
+stopWatch2.Stop();
+using(StreamWriter plistwriter = new(Console.OpenStandardOutput()))
+    {
+    foreach (int p in primeList)
+    {
+        // Console.Write(p+", ");
+        plistwriter.Write(p + ", ");
+    }
 }
 Console.WriteLine("\n"+primeList.Count+" Prime Numbers");
 
 stopWatch.Stop();
 TimeSpan ts = stopWatch.Elapsed;
+TimeSpan ts2 = stopWatch2.Elapsed;
 string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:0000000000}",
             ts.Hours, ts.Minutes, ts.Seconds,
             ts.Milliseconds);
-
+string elapsedTime2 = String.Format("{0:00}:{1:00}:{2:00}.{3:0000000000}",
+            ts2.Hours, ts2.Minutes, ts2.Seconds,
+            ts2.Milliseconds);
 Console.WriteLine(elapsedTime + " seconds");
+Console.WriteLine(elapsedTime2 + " seconds");
