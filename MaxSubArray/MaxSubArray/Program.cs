bc3ed9116ec594415ae0037338470aaecdc0db9c
Ronald Rowe <ronaldgilesrowe@gmail.com>
2022-06-11 09:20:56 -0500

    Converted to leet code problems from Python to C#

---
+++ MaxSubArray/MaxSubArray/Program.cs
@@ -0,0 +1,42 @@
+﻿class MaxSubArray
+{
+    public static void Main()
+    {
+        int[] numlst = { 3, 4, -10, 14, 2, -1 };
+        SubArray(numlst);
+    }
+
+
+    public static int SubArray(int[] nums)
+    { 
+        int maxtotal = nums[0];
+        int total;
+        int i = 0;
+        if (nums.Length == 1)
+        {
+            Console.WriteLine(maxtotal);
+            return maxtotal;
+        }
+        while (i < nums.Length)
+        {
+                       
+            total = nums[i];
+            if (total > maxtotal)
+            {
+                maxtotal = total;
+            }
+            foreach (var item in nums[(i+1)..])
+            {
+                total += item;
+                if (total > maxtotal)
+                {
+                    maxtotal=total;
+                }
+            }
+            i++;
+        }
+        Console.WriteLine(maxtotal);
+        return maxtotal;
+
+    }
+}
---
+++ MinStack/MinStack/Program.cs
@@ -0,0 +1,36 @@
+﻿class MinStack
+{
+    public Stack<int> stack = new ();
+    public Stack<int> minStack = new ();
+    
+
+
+    public void Push(int value)
+    {
+        this.stack.Push(value);
+        if ((!this.minStack.Any()) || value <= this.minStack.Peek())
+        {
+            this.minStack.Push(value);
+        }
+    }
+
+    public void Pop()
+    {
+        if (this.stack.Peek() == this.minStack.Peek())
+        {
+            this.minStack.Pop();
+        }
+        this.stack.Pop();
+    }
+
+    public int Top()
+    {
+        return this.stack.Peek();
+    }
+
+    public int GetMin()
+    {
+        return this.minStack.Peek();
+    }
+
+}
