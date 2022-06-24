d8b0c2a501bec47ff13bce909fee710fdf60c46f
Ronald Rowe <ronaldgilesrowe@gmail.com>
2022-06-24 18:30:38 -0500

    Add leetcode solutions

---
+++ MergeTwoSortedLists/MergeTwoSortedLists/Program.cs
@@ -0,0 +1,41 @@
+﻿public class Solution
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        //Merge Sorted Array, quite a bit slower than most, first try
+        //Decent memory usage, better than 77%
+        //Now Im wondering if working from high to low would have been better???
+        var firstPointer = 0;
+        var secondPointer = 0;
+        var mcount = m;
+        var ncount = n;
+
+        //Check to make sure pointer is not past the last number and that second list has values
+        while (firstPointer < mcount && ncount != 0)
+        {
+            if (nums1[firstPointer] <= nums2[secondPointer])
+            {//Increment firstPointer if smaller
+                firstPointer++;
+            }
+            else
+            {
+                for (var i = mcount - 1; i >= firstPointer; i--)
+                {//Shift values down
+                    nums1[i + 1] = nums1[i];
+                }
+                nums1[firstPointer] = nums2[secondPointer];//Add new value and increment everything
+                ncount--;
+                mcount++;
+                firstPointer++;
+                secondPointer++;
+            }
+        }
+        while (ncount > 0)
+        { //Catch the left over values after getting to the end of master list
+            nums1[firstPointer] = nums2[secondPointer];
+            firstPointer++;
+            secondPointer++;
+            ncount--;
+        }
+    }
+}
---
+++ TwoPointerComparrsion/TwoPointerComparrsion/Program.cs
@@ -0,0 +1,56 @@
+﻿public class Solution
+{
+    public int FirstUniqChar(string s)
+    {
+        if (s.Length == 1)
+        {
+            return 0;
+        }
+        var firstPointer = 0;
+        var endPointer = s.Length - 1;
+        while (firstPointer < endPointer)
+        {
+
+            if (s[firstPointer] == s[endPointer])
+            {
+                firstPointer++;
+                endPointer--;
+            }
+            else
+            {
+                var count = (endPointer - firstPointer) - 1;
+                foreach (var letter in s[(firstPointer + 1)..endPointer])
+                {
+                    if (letter == s[firstPointer])
+                    {
+                        break;
+                    }
+                    count--;
+                }
+
+                if (count == 0)
+                {
+                    return firstPointer;
+                }
+
+                var count2 = (endPointer - firstPointer) - 1;
+                foreach (var letter in s[(firstPointer + 1)..endPointer])
+                {
+                    if (letter == s[endPointer])
+                    {
+                        break;
+                    }
+                    count2--;
+                }
+                if (count2 == 0)
+                {
+                    return endPointer;
+                }
+                firstPointer++;
+                endPointer--;
+            }
+
+        }
+        return -1;
+    }
+}
